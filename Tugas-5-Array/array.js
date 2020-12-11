1b03831655d8b1e4f21a730baef4f7b48a266660
niafebriyanti <codeswithnia@gmail.com>
2020-12-11 23:15:34 +0700

    tugas array

---
+++ Tugas-5-Array/array.js
@@ -0,0 +1,54 @@
+console.log("TUGAS 5 - ARRAY\n");
+console.log("~~~ NOMOR 1 - Range ~~~\n");
+
+
+console.log(range(1, 10)) //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(1)) // -1
+console.log(range(11,18)) // [11, 12, 13, 14, 15, 16, 17, 18]
+console.log(range(54, 50)) // [54, 53, 52, 51, 50]
+console.log(range()) // -1 
+
+console.log("\n---------------------------------------------------\n");
+
+console.log("~~~ NOMOR 2 - Range with step ~~~\n");
+
+console.log(rangeWithStep(1, 10, 2)) // [1, 3, 5, 7, 9]
+console.log(rangeWithStep(11, 23, 3)) // [11, 14, 17, 20, 23]
+console.log(rangeWithStep(5, 2, 1)) // [5, 4, 3, 2]
+console.log(rangeWithStep(29, 2, 4)) // [29, 25, 21, 17, 13, 9, 5] 
+
+console.log("\n---------------------------------------------------\n");
+
+console.log("~~~ NOMOR 3 - Sum of range ~~~\n");
+
+console.log(sum(1,10)) // 55
+console.log(sum(5, 50, 2)) // 621
+console.log(sum(15,10)) // 75
+console.log(sum(20, 10, 2)) // 90
+console.log(sum(1)) // 1
+console.log(sum()) // 0 
+
+console.log("\n---------------------------------------------------\n");
+
+console.log("~~~ NOMOR 4 - Array multidimensi ~~~\n");
+
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+] 
+
+console.log("\n---------------------------------------------------\n");
+
+console.log("~~~ NOMOR 5 - Balik kata ~~~\n");
+
+console.log(balikKata("Kasur Rusak")) // kasuR rusaK
+console.log(balikKata("SanberCode")) // edoCrebnaS
+console.log(balikKata("Haji Ijah")) // hajI ijaH
+console.log(balikKata("racecar")) // racecar
+console.log(balikKata("I am Sanbers")) // srebnaS ma I 
+
+console.log("\n---------------------------------------------------\n");
+
+console.log("~~~ NOMOR 6 - Metode array~~~\n");
