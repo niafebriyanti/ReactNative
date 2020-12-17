790c9172fbf4e7b1cc5c136c87daf4150e322d32
niafebriyanti <codeswithnia@gmail.com>
2020-12-17 07:56:42 +0700

    tugas asynchronous

---
+++ Tugas-8-Asynchronous/index.js
@@ -0,0 +1,10 @@
+// di index.js
+var readBooks = require('./callback.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000}
+]
+ 
+// Tulis code untuk memanggil function readBooks di sini
